f686d52ab4c18e682b59a3624bbc8277d1a4a3d3
Dilmurod <dilmurod199911@gmail.com>
2021-06-26 23:21:55 +0500

    second commit

---
+++ app-expert-server/src/main/java/uz/developer/appexpertserver1/AppExpertServer1Application.java
@@ -0,0 +1,13 @@
+package uz.developer.appexpertserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExpertServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppExpertServer1Application.class, args);
+    }
+
+}
---
+++ app-expert-server/src/test/java/uz/developer/appexpertserver1/AppExpertServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.developer.appexpertserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExpertServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
